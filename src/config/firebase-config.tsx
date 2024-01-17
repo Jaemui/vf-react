e137d0f2f9e04b6eec5086afd65493c6ec909670
Josh T <joshatra@usc.edu>
2024-01-17 13:24:29 -0800

    setup

---
+++ src/config/firebase-config.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import "firebase/auth"
+
+const config ={
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_APP_MEASUREMENT_ID
+}
+
+export function getFirebaseConfig() {
+  if (!config || !config.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.ts');
+  } else {
+    return config;
+  }
+}
