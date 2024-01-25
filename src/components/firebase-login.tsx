58bcbf3869076abb82f3d4dad30b87f36eae4edc
Josh T <joshatra@usc.edu>
2024-01-24 19:48:45 -0800

    Created firebase-login component used in Login page

---
+++ src/components/firebase-login.tsx
@@ -0,0 +1,82 @@
+import  React  from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Button, TextField, Box, Typography, Link, Container } from '@mui/material';
+import { signInUser } from '../config/firebase';
+import { useNavigate } from 'react-router-dom';
+
+const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email format').required('Required'),
+    password: Yup.string().required('Required'),
+  });
+  
+  const FirebaseLogin = () => {
+    const navigate = useNavigate();
+    const formik = useFormik({
+      initialValues: {
+        email: '',
+        password: '',
+      },
+      validationSchema: validationSchema,
+      onSubmit: async (values) => {
+        try {
+          await signInUser(values.email, values.password);
+          console.log("successfully logged into fantasy valorant")
+          navigate('/profile');
+        } catch (error) {
+          alert('Failed to login');
+        }
+      },
+    });
+    return (
+    <Container sx = {{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh', width: '100vw'}} >
+        <Box sx={{ maxWidth: 600, mx: 'auto', display: 'flex', flexDirection: 'column' , alightItems: 'center', gap: 2 }}>
+            <img src = "/static/headers/app name (1).png" />
+            <form onSubmit={formik.handleSubmit}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Username/email"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.touched.password && Boolean(formik.errors.password)}
+                    helperText={formik.touched.password && formik.errors.password}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Login
+                </Button>
+            </form>
+            <Typography variant="body1" sx={{ mt: 2, textAlign: 'center' }}>
+            Don’t have an account? <Link href="/register">Create one</Link>
+            </Typography>
+        </Box>
+      </Container>
+    );
+  };
+
+export default FirebaseLogin;
+export{};
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,76 +1,12 @@
 
-import { ChangeEvent, FormEvent, useState } from 'react'
-import { signInUser } from '../config/firebase'
-import { useNavigate } from 'react-router-dom'
-
-const defaultFormFields = {
-  email: '',
-  password: '',
-}
+import FirebaseLogin from '../components/firebase-login';
 
 function Login() {
-  const [formFields, setFormFields] = useState(defaultFormFields)
-  const { email, password } = formFields
-  const navigate = useNavigate()
-
-  const resetFormFields = () => {
-    return (
-      setFormFields(defaultFormFields)
-    );
-  }
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-
-    try {
-      // Send the email and password to firebase
-      const userCredential = await signInUser(email, password)
-
-      if (userCredential) {
-        resetFormFields()
-        navigate('/profile')
-      }
-    } catch (error:any) {
-      console.log('User Sign In Failed', error.message);
-    }
-  };
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target
-    setFormFields({...formFields, [name]: value })
-  }
-
-  return(
-    <div className="App">
-      <div className="card">
-        <form onSubmit={handleSubmit}>
-          <div>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={handleChange}
-              placeholder="Email"
-              required
-            />
-          </div>
-          <div>
-            <input
-              type='password'
-              name='password'
-              value={password}
-              onChange={handleChange}
-              placeholder="Password"
-              required
-            />
-          </div>
-          <div>
-            <input id='recaptcha' type="submit" />
-          </div>
-        </form>
-      </div>
+  return (
+    <div>
+      <FirebaseLogin />
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
